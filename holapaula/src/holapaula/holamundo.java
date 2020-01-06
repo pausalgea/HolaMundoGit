392554d04136a593412027fb7c673ceab3525d3c
Paula Salicio <Paula Salicio@DESKTOP-7H8CE43>
2020-01-06 22:00:40 +0100

    primer commit

---
+++ holapaula/src/holapaula/holamundo.java
@@ -0,0 +1,31 @@
+package holapaula;
+
+public class holamundo {
+
+	static int edad=15;
+	static double d=5.943;
+	static float f=(3/4);
+	static char c='r';
+	static String cadena="HOLAAD DA";
+	
+
+
+	public holamundo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(cadena);
+		System.out.println(edad);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(c);
+		
+	}
+
+	
+
+	
+
+}
